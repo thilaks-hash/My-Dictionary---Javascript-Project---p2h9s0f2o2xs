83715126c97a7d5845013e0d318c070400348f78
thilaks-hash <thilagavathyr025@gmail.com>
2023-06-13 22:39:58 +0530

    updated

--- App.js
+++ App.js
@@ -9,14 +9,14 @@ const historybutton = document.getElementById("searchHistory-btn");
 const searchList = document.getElementById("historylist");
 var historyList = null;
 var searchlist = [];
-window.addEventListener("load", function () {
-  var savedhistory = localStorage.getItem("searchlist");
-  if (savedhistory) {
-    searchlist = JSON.parse(savedhistory);
-    createHistorylist();
-    renderHistory();
-  }
-});
+// window.addEventListener("load", function () {
+//   var savedhistory = localStorage.getItem("searchlist");
+//   if (savedhistory) {
+//     searchlist = JSON.parse(savedhistory);
+//     createHistorylist();
+//     renderHistory();
+//   }
+// });
 async function search() {
   try {
     const response = await fetch(
@@ -24,80 +24,78 @@ async function search() {
     );
     const data = await response.json();
 
-    displayData(data);
+    var searchhistory = searchInput.value.trim();
+
+    displayData(data, searchhistory);
   } catch (error) {
     console.log(error);
   }
 
   var searchhistory = searchInput.value.trim();
-  console.log(data, "dattaaaa");
+
   if (searchhistory !== "") {
-    addhistory(searchhistory);
     searchInput.value = "";
     savedhistory();
   }
 }
-const displayData = (data) => {
+const displayData = (data, searchhistory) => {
   const wordCard = document.createElement("p");
 
   if (typeof data == "object" && data.title) {
-    word.textContent = data.title;
+    const result = data.title;
+    word.textContent = result;
     wordCard.appendChild(word);
     resultCard.appendChild(wordCard);
+    addhistory(searchhistory, result);
   } else {
     const heading = data[0].word;
     word.textContent = heading;
     wordCard.appendChild(word);
     resultCard.appendChild(wordCard);
 
-    //idk why you are using this check... Whats the point of this condition?
-    // I'll comment this line for now
-    // If you added this line for a reason you can un comment it
-
-    // const meaning = data[0].meanings[1]
-    //   ? data[0]?.meanings[1]?.definitions[0]?.definition
-    //   : data[0]?.meanings[2]?.definitions[0]?.definition;
-
     const meaning = data[0]?.meanings[0]?.definitions[0]?.definition;
     para.innerText = meaning;
     wordCard.appendChild(para);
     resultCard.appendChild(wordCard);
-    const audCard = document.createElement("p");
     const audio_1 = data[0].phonetics[1].audio;
-    let aud = `<audio src="${data[0].phonetics[0].audio}" controls>`;
-    audCard.innerHTML = aud;
-    wordCard.appendChild(audCard);
+    audio.src = audio_1;
+    audio.controls = true;
+    wordCard.appendChild(audio);
     resultCard.appendChild(wordCard);
+    addhistory(searchhistory, meaning);
   }
 };
 
-function addhistory(text) {
-  console.log(text);
-  if (!historyList) {
-    createHistorylist();
-    console.log("add hist");
-  }
+function addhistory(text, meaning) {
+  // if (!historyList) {
+  //   createHistorylist();
+  // }
   var history = { text: text, meaning: meaning };
-  searchlist.push(history);
-  renderHistorylist(history);
-}
-function renderHistory() {
-  searchlist.forEach(function (history) {
-    renderHistorylist(history);
-  });
+  console.log(text, meaning);
+  console.log(history, "history");
+  searchlist.push({ text, meaning });
+  // renderHistorylist(history);
 }
 
-function renderHistorylist(history) {
-  var li = document.createElement("li");
-  var textNode = document.createTextNode(history.text);
-  li.appendChild(textNode);
-  historyList.appendChild(li);
-}
-function createHistorylist() {
-  historyList = document.createElement("ul");
-  historyList.style.display = "none";
-  searchList.appendChild(historyList);
-}
+//  function renderHistory() {
+//   searchlist.forEach(function (history) {
+//          renderHistorylist(history);
+//    });
+//  }
+
+// function renderHistorylist(history) {
+//   var li = document.createElement("li");
+//   var textNode = document.createTextNode(history.text);
+//   li.appendChild(textNode);
+//   historyList.appendChild(li);
+// }
+// function createHistorylist() {
+//   historyList = document.createElement("ul");
+//   historyList.style.display = "none";
+//   // console.log(historyList, "historyList");
+//   searchList.appendChild(historyList);
+//   console.log(searchList);
+// }
 function savedhistory() {
   localStorage.setItem("searchlist", JSON.stringify(searchlist));
 }
--- SearchHistory.js
+++ SearchHistory.js
@@ -1,15 +1,38 @@
 var histroyList = document.getElementById("histroylist");
 var searchList = localStorage.getItem("searchlist");
-
-window.addEventListener("load", function () {
+var searchCard = window.addEventListener("load", function () {
   if (searchList) {
     searchList = JSON.parse(searchList);
 
     searchList.forEach(function (searches) {
       console.log(searches);
+      let texts = searches.text;
       var listItem = document.createElement("li");
+      var listdef = document.createElement("p");
       listItem.textContent = searches.text;
+      listdef.textContent = searches.meaning;
       histroyList.appendChild(listItem);
+      histroyList.appendChild(listdef);
+      const deletebutton = document.createElement("button");
+      deletebutton.setAttribute("id", "deletebutton");
+      deletebutton.innerHTML = "delete";
+      histroyList.appendChild(deletebutton);
+      deletebutton.addEventListener("click", () => {
+        var itemRemoved = listdef.textContent;
+        listdef.innerText = "";
+
+        for (let i = 0; i < searchList.length; i++) {
+          if (searchList[i].meaning === itemRemoved) {
+            var item = localStorage.key(i);
+
+            localStorage.removeItem(item);
+          }
+        }
+
+        let temp = searchList.filter((item) => item.text != texts);
+        localStorage.setItem("searchlist", JSON.stringify(temp));
+        // delete localStorage.searches.text;
+      });
     });
   }
 });
