414f41751dfe88212157ca410360aac65a209b53
thilaks-hash <thilagavathyr025@gmail.com>
2023-06-19 23:23:16 +0530

    updated

--- App.js
+++ App.js
@@ -1,104 +1,68 @@
-const searchInput = document.getElementById("search");
-const resultCard = document.getElementById("cards");
+document.addEventListener("DOMContentLoaded", function () {
+  const searchInput = document.getElementById("search");
+  const resultCard = document.getElementById("cards");
+  const searchButton = document.getElementById("search-btn");
+  const word = document.createElement("h2");
+  const para = document.createElement("p");
+  const audio = document.createElement("audio");
+  //var searchlist = [];
+  searchButton.addEventListener("click", handleSearch);
+  async function handleSearch() {
+    try {
+      const response = await fetch(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${searchInput.value.toLowerCase()}`
+      );
+      const data = await response.json();
+      console.log(data);
 
-const word = document.createElement("h2");
-const para = document.createElement("p");
-const audio = document.createElement("audio");
-const historybutton = document.getElementById("searchHistory-btn");
-const searchList = document.getElementById("historylist");
-var historyList = null;
-var searchlist = [];
-window.addEventListener("load", function () {
-  var savedhistory = localStorage.getItem("searchlist");
-  if (savedhistory) {
-    searchlist = JSON.parse(savedhistory);
-    createHistorylist();
-    renderHistory();
-  }
-});
-async function search() {
-  try {
-    const response = await fetch(
-      `https://api.dictionaryapi.dev/api/v2/entries/en/${searchInput.value.toLowerCase()}`
-    );
-    const data = await response.json();
-    console.log(data);
+      var searchhistory = searchInput.value.trim();
+      displayData(data, searchhistory);
+    } catch (error) {
+      console.log(error);
+    }
 
     var searchhistory = searchInput.value.trim();
 
-    displayData(data, searchhistory);
-  } catch (error) {
-    console.log(error);
+    if (searchhistory !== "") {
+      searchInput.value = "";
+      // savedhistory();
+    }
   }
+  const displayData = (data, searchhistory) => {
+    if (typeof data == "object" && data.title) {
+      resultCard.innerText = "";
+      const result = data.title;
+      word.textContent = result;
+      const parag = document.createElement("p");
+      parag.appendChild(word);
 
-  var searchhistory = searchInput.value.trim();
+      resultCard.appendChild(parag);
+      savedhistory(searchhistory, result);
+    } else {
+      const heading = data[0].word;
+      word.textContent = heading;
+      const wordCard = document.createElement("p");
+      wordCard.appendChild(word);
+      const meaning = data[0]?.meanings[0]?.definitions[0]?.definition;
+      para.innerText = meaning;
+      wordCard.appendChild(para);
+      const audio_1 = data[0].phonetics[1].audio;
+      audio.src = audio_1;
+      audio.controls = true;
+      audio.style.backgroundColor = "black";
+      audio.style.padding = "5px";
+      wordCard.appendChild(audio);
+      resultCard.appendChild(wordCard);
+      resultCard.setAttribute("class", "resultcard");
+      const searchList = JSON.parse(localStorage.getItem("searchList")) || [];
+      console.log(searchList, "searchList");
+      savedhistory(searchhistory, meaning);
+    }
+  };
 
-  if (searchhistory !== "") {
-    searchInput.value = "";
-    savedhistory();
+  function savedhistory(text, meaning) {
+    let searchList = JSON.parse(localStorage.getItem("searchList")) || [];
+    searchList.push({ text, meaning });
+    localStorage.setItem("searchList", JSON.stringify(searchList));
   }
-}
-const displayData = (data, searchhistory) => {
-  if (typeof data == "object" && data.title) {
-    resultCard.innerText = "";
-    const result = data.title;
-    word.textContent = result;
-    const parag = document.createElement("p");
-    parag.appendChild(word);
-
-    resultCard.appendChild(parag);
-    addhistory(searchhistory, result);
-  } else {
-    const heading = data[0].word;
-    word.textContent = heading;
-    const wordCard = document.createElement("p");
-    wordCard.appendChild(word);
-    const meaning = data[0]?.meanings[0]?.definitions[0]?.definition;
-    para.innerText = meaning;
-    wordCard.appendChild(para);
-    const audio_1 = data[0].phonetics[1].audio;
-    audio.src = audio_1;
-    audio.controls = true;
-    audio.style.backgroundColor = "black";
-    audio.style.padding = "5px";
-    wordCard.appendChild(audio);
-    resultCard.appendChild(wordCard);
-    resultCard.setAttribute("class", "resultcard");
-    const searchList = JSON.parse(localStorage.getItem("searchlist")) || [];
-    var index = searchList.length;
-    console.log(searchList, "searchList");
-    addhistory(searchhistory, meaning, index);
-  }
-};
-
-function addhistory(text, meaning, index) {
-  if (!historyList) {
-    createHistorylist();
-  }
-  var history = { text: text, meaning: meaning };
-  searchlist.push({ index, text, meaning });
-  renderHistorylist(history);
-}
-
-function renderHistory() {
-  searchlist.forEach(function (history) {
-    renderHistorylist(history);
-  });
-}
-
-function renderHistorylist(history) {
-  var li = document.createElement("li");
-  var textNode = document.createTextNode(history.text);
-  li.appendChild(textNode);
-  historyList.appendChild(li);
-}
-function createHistorylist() {
-  historyList = document.createElement("ul");
-  historyList.style.display = "none";
-  console.log(historyList, "historyList");
-  searchlist.appendChild(historyList);
-  console.log(searchList);
-}
-function savedhistory() {
-  localStorage.setItem("searchlist", JSON.stringify(searchlist));
-}
+});
--- SearchHistory.js
+++ SearchHistory.js
@@ -1,46 +1,42 @@
-var histroyList = document.getElementById("histroylist");
-var searchList = localStorage.getItem("searchlist");
-function clearAll() {
-  localStorage.removeItem("searchlist");
-  histroyList.style.display = "none";
-  searchlist = [];
-}
-function deleteItemFromLocalStorage(index) {
-  const storedItems = JSON.parse(localStorage.getItem("searchlist")) || [];
-  storedItems.splice(index, 1);
-  for (let i = index; i < storedItems.length; i++) {
-    storedItems[i].index = i;
-    console.log(storedItems.length);
-  }
-  localStorage.setItem("searchlist", JSON.stringify(storedItems));
+document.addEventListener("DOMContentLoaded", function () {
+  var histroyList = document.getElementById("histroylist");
+  showSearchHistory();
 
-  //const updatedItems = storedItems.filter((item) => item.index !== index);
-  //console.log(updatedItems, "updatedItems");
-}
+  function showSearchHistory() {
+    var searchList = JSON.parse(localStorage.getItem("searchList")) || [];
+    if (searchList.length >= 0) {
+      histroyList.innerHTML = "";
 
-var searchCard = window.addEventListener("load", function () {
-  if (searchList) {
-    searchList = JSON.parse(searchList);
-    searchList.forEach(function (searches, index) {
-      var listItem = document.createElement("li");
-      listItem.style.listStyle = "none";
-      var listdef = document.createElement("p");
-      var historylist1 = document.createElement("div");
-      listItem.textContent = searches.text;
-      listdef.textContent = searches.meaning;
-      historylist1.appendChild(listItem);
-      historylist1.appendChild(listdef);
-      const deletebutton = document.createElement("button");
-      deletebutton.setAttribute("id", "deletebutton");
-      deletebutton.innerHTML = "delete";
-      historylist1.appendChild(deletebutton);
-      histroyList.appendChild(historylist1);
-      historylist1.setAttribute("class", "resultcard");
-      deletebutton.addEventListener("click", () => {
-        deleteItemFromLocalStorage(index);
-        console.log(historylist1);
-        historylist1.style.display = "none";
+      searchList.forEach((searches) => {
+        var historylist1 = document.createElement("div");
+
+        var listItem = document.createElement("li");
+        listItem.style.listStyle = "none";
+
+        var listdef = document.createElement("p");
+        var historylist1 = document.createElement("div");
+        listItem.textContent = searches.text;
+        historylist1.appendChild(listItem);
+
+        listdef.textContent = searches.meaning;
+        historylist1.appendChild(listdef);
+
+        const deletebutton = document.createElement("button");
+        deletebutton.setAttribute("id", "deletebutton");
+        deletebutton.innerHTML = "delete";
+        deletebutton.addEventListener("click", () => {
+          deleteItemFromLocalStorage(searchList.indexOf(searches));
+        });
+        historylist1.appendChild(deletebutton);
+        histroyList.appendChild(historylist1);
+        historylist1.setAttribute("class", "resultcard");
       });
-    });
+    }
+  }
+  function deleteItemFromLocalStorage(index) {
+    let storedItems = JSON.parse(localStorage.getItem("searchList")) || [];
+    storedItems.splice(index, 1);
+    localStorage.setItem("searchList", JSON.stringify(storedItems));
+    showSearchHistory();
   }
 });
